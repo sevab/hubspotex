2f6f3a489dddd85df593ecb2de5bee12d741801b
Tom Maszk <tmaszk@pobox.com>
2022-09-19 13:43:16 -0400

    update packages

--- mix.exs
+++ mix.exs
@@ -29,8 +29,8 @@ defmodule Hubspot.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:httpoison, "~> 0.9.0"},
-     {:poison, "~> 2.0"},
+    [{:httpoison, "~> 1.0"},
+     {:poison, "~> 5.0"},
      {:ex_doc, ">= 0.0.0", only: :dev}]
   end
 
